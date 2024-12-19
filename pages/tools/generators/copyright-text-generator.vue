b880469d4f31be166eaaa7703ab920b6f0832b38
Axuata <axuata@gmail.com>
2024-12-19 14:04:51 +0900

    ライセンスの選択肢を追加

--- pages/tools/generators/copyright-text-generator.vue
+++ pages/tools/generators/copyright-text-generator.vue
@@ -3,6 +3,7 @@ const firstCreatedYear = ref<string>('');
 const lastUpdatedYear = ref<string>('');
 const ownerName = ref<string>('');
 const isAllRightsReserved = ref<boolean>(false);
+const selectedLicense = ref<string>('');
 
 let resultParagraphElement: HTMLParagraphElement | null = null;
 
@@ -13,9 +14,29 @@ onMounted(() => {
 function generate(): void {
   if (resultParagraphElement) {
     if (isAllRightsReserved.value) {
-      resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value} - All Rights Reserved.`;
+      switch (selectedLicense.value) {
+        case "なし":
+          resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value} - All Rights Reserved.`;
+          break;
+        case "MIT License":
+          resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value} - All Rights Reserved. - This project is licensed under the MIT License.`;
+          break;
+        default:
+          resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value} - All Rights Reserved.`;
+          break;
+      }
     } else {
-      resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value}`;
+      switch (selectedLicense.value) {
+        case "なし":
+          resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value}`;
+          break;
+        case "MIT License":
+          resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value} - This project is licensed under the MIT License.`;
+          break;
+        default:
+          resultParagraphElement.textContent = `© ${firstCreatedYear.value} - ${lastUpdatedYear.value} ${ownerName.value}`;
+          break;
+      }
     }
   }
 }
@@ -27,6 +48,7 @@ function generate(): void {
     <CBaseInput v-model="lastUpdatedYear" label="最後に更新された年（西暦）" caution="年を入力してください" is-required />
     <CBaseInput v-model="ownerName" label="著作者名" caution="名前を入力してください" is-required />
     <CBaseSwitch v-model="isAllRightsReserved" label="All Rights Reserved" caution="" is-required switch-label="ON/OFF" />
+    <CBaseRadio v-model="selectedLicense" label="ライセンス" caution="どれか一つだけ選んでください" is-required :checkboxes="['なし', 'MIT License']" />
     <CBaseHorizontalLine />
     <CBaseButton label="生成" @click="generate" />
     <CBaseHorizontalLine />
