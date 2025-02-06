5b3aaac49bb3e1b3f680a3b93ce10013d57be792
Axuata <axuata@gmail.com>
2025-02-06 18:11:59 +0900

    Footerを変更

--- components/layout/CFooter.vue
+++ components/layout/CFooter.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="py-20px font-inter text-gray-6">
-    <p>&copy; 2024 Axuata. This project is licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>.</p>
+    <p>&copy; 2024-2025 Axuata. This project is licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>.</p>
   </div>
 </template>
 
